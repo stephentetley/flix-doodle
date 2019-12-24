ab58bcdd48ad616696ed411225420bc41a8893fe
stephentetley <stephen.tetley@gmail.com>
2019-12-24 21:27:22 +0000

    StringBuilder - added 'appenders' for other numeric types(Int8, Int16, Int64, ...), plus work on tests and wrappers.

--- string-builder/java/src/flix/runtime/library/MonoStringBuilder.java
+++ string-builder/java/src/flix/runtime/library/MonoStringBuilder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Stephen Tetley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package flix.runtime.library;
 
@@ -32,13 +47,23 @@ public class MonoStringBuilder {
         return;
     }
 
+    public void appendByte(byte i) {
+        this.sb.append(i);
+        return;
+    }
+
+    public void appendShort(short i) {
+        this.sb.append(i);
+        return;
+    }
+
     public void appendInt(int i) {
         this.sb.append(i);
         return;
     }
 
-    public void appendDouble(double d) {
-        this.sb.append(d);
+    public void appendLong(long i) {
+        this.sb.append(i);
         return;
     }
 
@@ -47,14 +72,18 @@ public class MonoStringBuilder {
         return;
     }
 
+    public void appendDouble(double d) {
+        this.sb.append(d);
+        return;
+    }
+
+
+
     public void appendLineSeparator() {
         String s = System.lineSeparator();
         this.sb.append(s);
         return;
     }
 
-    public void reverse() {
-        this.sb.reverse();
-        return;
-    }
+
 }
