78c0f44ae1c199ac4919b4f37a59924c56b675ec
stephentetley <stephen.tetley@gmail.com>
2019-12-28 12:02:48 +0000

    String Builder - tidying code, plus added append function for BigInt.

--- string-builder/java/src/flix/runtime/library/MonoStringBuilder.java
+++ string-builder/java/src/flix/runtime/library/MonoStringBuilder.java
@@ -16,62 +16,85 @@
 
 package flix.runtime.library;
 
-import java.lang.*;
+import java.lang.StringBuilder;
+import java.math.BigInteger;
 
+/**
+ * Wrap Java's StringBuilder, provide a small, monomorphic API.
+ *
+ * Note - Java's StringBuilder is imperative, appending elements
+ * mutates the original StringBuilder.
+ */
 public class MonoStringBuilder {
     private StringBuilder sb = new StringBuilder();
 
+    // Convert to a String
     public String toString() {
         return sb.toString();
     }
 
-
+    // Append a String
     public void appendString(String s) {
         this.sb.append(s);
         return;
     }
 
+    // Append a Char
     public void appendChar(char c) {
         this.sb.append(c);
         return;
     }
 
+    // Append a byte (int8)
     public void appendByte(byte i) {
         this.sb.append(i);
         return;
     }
 
+    // Append a short (int16)
     public void appendShort(short i) {
         this.sb.append(i);
         return;
     }
 
+    // Append an int (int32)
     public void appendInt(int i) {
         this.sb.append(i);
         return;
     }
 
+    // Append a long (int64)
     public void appendLong(long i) {
         this.sb.append(i);
         return;
     }
 
+    // Append a BigInteger (BigInt)
+    public void appendBigInteger(BigInteger i) {
+        this.sb.append(i);
+        return;
+    }
+
+    // Append a float (float32)
     public void appendFloat(float d) {
         this.sb.append(d);
         return;
     }
 
+    // Append a double (float64)
     public void appendDouble(double d) {
         this.sb.append(d);
         return;
     }
 
+    // Append a newline
     public void appendLineSeparator() {
         String s = System.lineSeparator();
         this.sb.append(s);
         return;
     }
 
+    // Return the length of the StringBuilder
     public int length() {
         return this.sb.length();
     }
