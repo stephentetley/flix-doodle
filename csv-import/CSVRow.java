d68e27dcbfe1852284afc5ff38f3f4fce00f7284
stephentetley <stephen.tetley@gmail.com>
2020-01-10 18:59:07 +0000

    csv-import - added more primitive cell readers.

--- csv-import/CSVRow.java
+++ csv-import/CSVRow.java
@@ -2,7 +2,7 @@ package flix.runtime.csvimport;
 
 import org.apache.commons.csv.CSVRecord;
 
-import java.util.Scanner;
+import java.math.BigInteger;
 
 public class CSVRow {
     private CSVRecord wrappedRow;
@@ -11,20 +11,43 @@ public class CSVRow {
         this.wrappedRow = row;
     }
 
-    public String getString(int i) {
-        return this.wrappedRow.get(i);
+    public String getString(int ix) {
+        return this.wrappedRow.get(ix);
     }
 
-    public int getInt(int i) throws Exception {
-        String input = this.wrappedRow.get(i);
-        Scanner scanner = new Scanner(input);
-        if (scanner.hasNextInt()) {
-            return scanner.nextInt();
-        } else {
-            throw new Exception();
-        }
+    public byte getByte(int ix) throws Exception {
+        String input = this.wrappedRow.get(ix);
+        return Byte.parseByte(input.trim());
+    }
 
+    public short getShort(int ix) throws Exception {
+        String input = this.wrappedRow.get(ix);
+        return Short.parseShort(input.trim());
+    }
 
+    public int getInt(int ix) throws Exception {
+        String input = this.wrappedRow.get(ix);
+        return Integer.parseInt(input.trim());
+    }
+
+    public long getLong(int ix) throws Exception {
+        String input = this.wrappedRow.get(ix);
+        return Long.parseLong(input.trim());
+    }
+
+    public float getFloat(int ix) throws Exception {
+        String input = this.wrappedRow.get(ix);
+        return Float.parseFloat(input.trim());
+    }
+
+    public double getDouble(int ix) throws Exception {
+        String input = this.wrappedRow.get(ix);
+        return Double.parseDouble(input.trim());
+    }
+
+    public BigInteger getBigInteger(int ix) throws Exception {
+        String input = this.wrappedRow.get(ix);
+        return new BigInteger(input.trim());
     }
 
 }
