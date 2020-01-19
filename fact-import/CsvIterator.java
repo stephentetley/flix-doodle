16acda38321f3ef0c92bb44740c1307a12a82999
stephentetley <stephen.tetley@gmail.com>
2020-01-19 21:37:01 +0000

    Work on Csv Importer including name changes so it can coexist with a SQLite importer.

---
+++ fact-import/CsvIterator.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Stephen Tetley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package flix.runtime.factimport;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
+public class CsvIterator {
+
+    private Iterator<CSVRecord> rator;
+
+    public CsvIterator(String filepath) throws Exception {
+        FileReader in = new FileReader(filepath);
+        Iterable<CSVRecord> rable = CSVFormat.EXCEL.parse(in);
+        rator = rable.iterator();
+    }
+
+    public boolean hasNext() {
+        return rator.hasNext();
+    }
+
+    public CsvRow next() {
+        CsvRow row = new CsvRow(rator.next());
+        return row;
+    }
+}
--- fact-import/CsvRow.java
+++ fact-import/CsvRow.java
@@ -1,13 +1,29 @@
-package flix.runtime.csvimport;
+/*
+ * Copyright 2020 Stephen Tetley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package flix.runtime.factimport;
 
 import org.apache.commons.csv.CSVRecord;
 
 import java.math.BigInteger;
 
-public class CSVRow {
+public class CsvRow {
     private CSVRecord wrappedRow;
 
-    public CSVRow(CSVRecord row) {
+    public CsvRow(CSVRecord row) {
         this.wrappedRow = row;
     }
 
